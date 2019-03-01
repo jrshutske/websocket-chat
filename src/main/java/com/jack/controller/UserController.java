e45fc1ef09195c9fd24ba4867a6b5b559d6e6775
jrshutske <jackshutske@gmail.com>
2019-03-01 14:03:18 -0600

    clean up UI, add new, and create, fix forms

--- src/main/java/com/jack/controller/UserController.java
+++ src/main/java/com/jack/controller/UserController.java
@@ -15,24 +15,24 @@ import java.util.List;
 @Controller
 @SpringBootApplication
 public class UserController {
-    @GetMapping("/users")
+    @GetMapping("/user")
     String users(Model model) {
         UserDao userDao = new UserDao();
         List<User> rs = userDao.getAll();
         model.addAttribute("users", rs);
-        return "users.html";
-    }
-
-    @GetMapping("/user/{id}")
-    String userbyid(@PathVariable int id, Model model) {
-        UserDao userDao = new UserDao();
-        User user = userDao.getById(id);
-        model.addAttribute("user", user);
         return "user";
     }
 
+    @GetMapping("/user/{id}")
+    String getuserbyid(@PathVariable int id, Model model) {
+        UserDao userDao = new UserDao();
+        User user = userDao.getById(id);
+        model.addAttribute("user", user);
+        return "usershow";
+    }
+
     @GetMapping("/user/{id}/edit")
-    String edituserbyid(@PathVariable int id, Model model) {
+    String edituser(@PathVariable int id, Model model) {
         UserDao userDao = new UserDao();
         User user = userDao.getById(id);
         model.addAttribute("user", user);
@@ -40,11 +40,43 @@ public class UserController {
     }
 
     @PostMapping(value = "/user/{id}/update")
-    String submit(@PathVariable int id, @RequestParam("userName") String userName) {
+    String updateuser(@PathVariable int id,
+                      @RequestParam("userName") String userName,
+                      @RequestParam("firstName") String firstName,
+                      @RequestParam("lastName") String lastName,
+                      @RequestParam("password") String password) {
         UserDao userDao = new UserDao();
         User user = userDao.getById(id);
         user.setUserName(userName);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setPassword(password);
         userDao.saveOrUpdate(user);
+        return "redirect:/user/{id}/show";
+    }
+
+    @GetMapping("/user/new")
+    String newuser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "usernew";
+    }
+
+    @PostMapping(value = "/user/create")
+    String createuser(@RequestParam("userName") String userName,
+                      @RequestParam("firstName") String firstName,
+                      @RequestParam("lastName") String lastName,
+                      @RequestParam("email") String email,
+                      @RequestParam("password") String password) {
+        UserDao userDao = new UserDao();
+        User user = new User();
+        user.setUserName(userName);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setPassword(password);
+        userDao.saveOrUpdate(user);
+        int id = user.getId();
         return "redirect:/user/{id}";
 
     }
